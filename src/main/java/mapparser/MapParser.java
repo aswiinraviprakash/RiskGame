1a15f0a4d2a88d5147662b20dc7e249de0528d96
dashi1601 <dakshi1601@gmail.com>
2023-10-01 17:46:00 -0400

    Added methods to MapEditor class

--- src/main/java/mapparser/MapParser.java
+++ src/main/java/mapparser/MapParser.java
@@ -1,14 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
 package mapparser;
+
+import constants.GameMessageConstants;
+import gameutils.MapException;
 
 /**
  *
  * @author USER
  */
 public class MapParser {
+    
+    public String d_map_file_name; 
+    
+    public MapParser(){
+        this.d_map_file_name = "D:\\Concordia\\Fall2023\\APP\\Project\\canada.map";
+        //if this does not exist throw error;
+    }
+    
+    public MapParser(String p_map_file_name){
+        this.d_map_file_name = p_map_file_name;
+    }
 
-   
+    public Map loadMap() {
+
+        //create map object
+        Map l_map = new Map(this.d_map_file_name);
+        
+        try{
+            if(l_map.d_borders == null || l_map.d_countries == null || l_map.d_continents == null){
+            throw new MapException(GameMessageConstants.D_MAP_LOAD_FAILED);
+        }
+        } catch(MapException e){
+            System.out.println(e.getMessage());
+        }catch (Exception e) {
+            System.out.println(GameMessageConstants.D_INTERNAL_ERROR);
+        }
+       
+        
+        return l_map;
+    }
+
 }
