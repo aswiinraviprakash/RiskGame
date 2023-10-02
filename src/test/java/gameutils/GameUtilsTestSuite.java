03673249259641b675119d30aab5567342528de0
AswiinR <aswiinr19@gmail.com>
2023-10-02 04:49:53 -0400

    junit test config and initial changes

---
+++ src/test/java/gameutils/GameUtilsTestSuite.java
@@ -0,0 +1,13 @@
+package gameutils;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+    GameCommandParserTest.class
+})
+
+public class GameUtilsTestSuite {
+}
