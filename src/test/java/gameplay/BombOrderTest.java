632adfe7262dd2472bf29c5423ace8bb7def0959
Sasikiran-31 <kirankumar.2001@yahoo.in>
2023-11-06 15:06:07 -0500

    BombOrder Test Case

---
+++ src/test/java/gameplay/BombOrderTest.java
@@ -0,0 +1,70 @@
+package gameplay;
+
+import mapparser.GameMap;
+import mapparser.LoadMapPhase;
+import constants.GameConstants;
+import org.junit.Test;
+import org.junit.Before;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import org.junit.Assert;
+
+
+
+public class BombOrderTest {
+    private GameInformation d_current_game_info;
+    private GameMap.Country d_destination_country;
+
+    @Before
+    public void initializeTestData() {
+        try {
+
+            d_current_game_info = GameInformation.getInstance();
+
+            String l_gamemap_filename = "valid-testmap.map";
+            LoadMapPhase l_loadmap_phase = new LoadMapPhase(l_gamemap_filename, false);
+            l_loadmap_phase.setMapDirectory(GameConstants.D_MAP_TEST_DIRECTORY);
+            l_loadmap_phase.executePhase();
+            mapparser.GameMap l_gamemap_obj = l_loadmap_phase.getLoadedMap();
+
+            d_current_game_info.setCurrenGameMap(l_gamemap_obj);
+
+            LinkedHashMap<String, Player> l_player_list = d_current_game_info.getPlayerList();
+            Player l_player_obj = new Player("sourceplayer");
+            l_player_list.put("sourceplayer", l_player_obj);
+
+            l_player_obj = new Player("targetplayer");
+            l_player_list.put("targetplayer", l_player_obj);
+            d_current_game_info.setPlayerList(l_player_list);
+
+
+
+        } catch (Exception e) {}
+    }
+
+
+    @Test
+    public void TestBombingEnemyTerritory(){
+        try{
+            LinkedHashMap<String, Player> l_player_list = d_current_game_info.getPlayerList();
+            Player l_player_first_obj = l_player_list.get("playerfirst");
+            Player l_player_Second_obj = l_player_list.get("playersecond");
+            List<GameMap.Country> l_countries = d_current_game_info.getGameMap().getCountryObjects();
+
+            l_player_first_obj.setConqueredCountries(Arrays.asList(new GameMap.Country[]{ l_countries.get(0), l_countries.get(3), l_countries.get(4)}));
+            l_player_Second_obj.setConqueredCountries(Arrays.asList(new GameMap.Country[]{ l_countries.get(1), l_countries.get(2), l_countries.get(5)}));
+
+            d_destination_country = l_player_Second_obj.getConqueredCountries().get(0);
+
+            BombOrder l_bomb_phase = new BombOrder(d_destination_country);
+            l_bomb_phase.execute(l_player_first_obj);
+
+            Assert.assertEquals(2, l_player_Second_obj.getConqueredCountries().get(0).getArmyCount());
+
+        } catch (Exception e) {}
+    }
+    
+}
