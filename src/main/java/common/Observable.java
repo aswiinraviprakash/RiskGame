a2c178d0c94d6f44b049bbdff67f0d4522b04dcc
Sasikiran-31 <kirankumar.2001@yahoo.in>
2023-11-08 19:13:12 -0500

    logger_jdocs

--- src/main/java/common/Observable.java
+++ src/main/java/common/Observable.java
@@ -3,17 +3,32 @@ package common;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Observable is an Abstract class used for performing observable methods
+ */
 public abstract class Observable {
     private List<Observer> d_observers = new ArrayList<>();
 
+    /**
+     * Method to Add Observers
+     * @param p_observer
+     */
     public void addObserver(Observer p_observer) {
         this.d_observers.add(p_observer);
     }
 
+    /**
+     * Method to remove Observers
+     * @param p_observer
+     */
     public void removeObserver(Observer p_observer) {
         if (!this.d_observers.isEmpty()) this.d_observers.remove(p_observer);
     }
 
+    /**
+     * Method to notify Observers
+     * @param p_observable
+     */
     public void notifyObservers(Observable p_observable) {
         for (Observer l_observer : this.d_observers) {
             l_observer.update(p_observable);
