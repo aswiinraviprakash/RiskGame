1d6a7ee3591afcca6e1f6227e40e52734198d80d
Sasikiran-31 <kirankumar.2001@yahoo.in>
2023-11-23 20:16:08 -0500

    Save_Load2

--- src/main/java/common/SaveLoadGame.java
+++ src/main/java/common/SaveLoadGame.java
@@ -1,47 +1,51 @@
+package common;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+import constants.GameConstants;
+import gameplay.GameInformation;
+import gameutils.GameException;
+
 public class SaveLoadGame {
 
-    public boolean saveGame(GameInformation p_game_info){
-        boolean l_isSaved = false;
+    public void saveGame(String p_file_name) throws Exception {
         try{
             
-            checkDirectory("saveGameFile");
-            String l_save_file_path = "saveGameFile"+File.separator+GameConstants.D_SAVE_FILE_NAME; //need to create a file in Constants
+            checkDirectory();
+            String l_save_file_path = GameConstants.D_SAVE_DIRECTORY + File.separator + p_file_name + ".ser";
             ObjectOutputStream l_save = new ObjectOutputStream(new FileOutputStream(l_save_file_path));
-            l_save.writeObject(p_game_info);
+            l_save.writeObject(GameInformation.getInstance());
             l_save.close();
-            l_isSaved = true;
+            System.out.println("Game saved successfully!!");
+
         } catch (Exception e){
-            e.printStackTrace();
-            l_isSaved = false;
-            System.out.println("Error saving the game..");
+            throw new GameException("Error saving the game");
         }
-        return l_isSaved;
 
     }
 
-    public GameInformation loadGame(){
-        GameInformation l_game_info = new GameInformation();
+    public GameInformation loadGame(String p_file_name) throws Exception {
+        GameInformation l_game_info;
         try{
-            String l_save_file_path = "saveGameFile"+File.separator+GameConstants.D_SAVE_FILE_NAME;
-            FileInputStream l_saveFile =  new FileInputStream(l_save_file_path);
+            
+            String l_load_file_path = GameConstants.D_SAVE_DIRECTORY + File.separator + p_file_name + ".ser";
+            FileInputStream l_saveFile =  new FileInputStream(l_load_file_path);
             ObjectInputStream save = new ObjectInputStream(l_saveFile);
             l_game_info = (GameInformation) save.readObject();
             save.close();
         } catch (Exception e){
-            e.printStackTrace();
-            System.out.println("Error Loading the game...");
+            throw new GameException("Error loading the game");
         }
         return l_game_info;
     }
 
-    private void checkDirectory(String p_directory_path){
-        File l_file_directory = new File(p_directory_path);
+    private void checkDirectory(){
+        String l_file_path = GameConstants.D_SAVE_DIRECTORY;
+        File l_file_directory = new File(l_file_path);
         if(!l_file_directory.exists() || !l_file_directory.isDirectory()){
             l_file_directory.mkdirs();
         }
